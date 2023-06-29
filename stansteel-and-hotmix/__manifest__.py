a5332bfbfe6acd8ff6c34deb5afd2b62457c022f
tule-odoo <tule@odoo.com>
2023-06-29 09:36:16 -0400

    Created the module

---
+++ stansteel-and-hotmix/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "Stansteel & Hotmix",
+    
+    "summary": "Tu Le's individual task Day 1",
+    
+    "description": """
+    The customer wants to see the total cost of the products being sold on the SO line as well as the total cost of the SO
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Individual Task",
+    
+    "license": "OPL-1",
+    
+    "depends": ["base"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "tule",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+
+    "auto-install": True,
+    
+}
