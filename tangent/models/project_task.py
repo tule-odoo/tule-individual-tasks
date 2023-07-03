0be2f19ea984cc4313b647584013a46b9b91aedd
tule-odoo <tule@odoo.com>
2023-07-03 14:55:48 -0400

    Created draft invoice automatically from task page

--- tangent/models/project_task.py
+++ tangent/models/project_task.py
@@ -4,9 +4,28 @@ import random
 class ProjectTask(models.Model):
     _inherit = "project.task"
 
-    invoice_number = fields.Many2one(comodel_name="account.move", readonly=True, compute="_generate_invoice")
+    invoice_number = fields.Many2one(readonly=True, comodel_name="account.move", compute="_generate_invoice")
 
-    @api.onchange("x_studio_delivery_date")
+    @api.depends('sale_line_id')
     def _generate_invoice(self):
+        print("kjhkjhkjh")
         for task in self:
-            task.invoice_number = random.randint(0, 100)
+            print(task.sale_line_id.invoice_lines.id)
+            task.invoice_number = task.sale_line_id.invoice_lines[0].move_id
+
+    @api.model
+    def create(self, vals):
+        # Checking for the sale orders id and delivery date
+        if vals.get('sale_line_id') != False and vals.get('x_studio_delivery_date') != False:
+            # Incrementing delivered quantity
+            sale_order_line = self.env['sale.order.line'].search([('id', '=', vals.get('sale_line_id'))])
+            sale_order_line.write({'qty_delivered': 1.0})
+
+            # Creating the invoice
+            sale_order = sale_order_line.order_id
+            sale_order._create_invoices(grouped=True)
+            
+        return super(ProjectTask, self).create(vals)
+            
+
+            
