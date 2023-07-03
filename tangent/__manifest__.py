0ed02eb8e0d1c9129de39015b998113359b8c875
tule-odoo <tule@odoo.com>
2023-07-03 09:32:33 -0400

    Added __init__

--- tangent/__manifest__.py
+++ tangent/__manifest__.py
@@ -4,14 +4,7 @@
     "summary": "Invoice Created from Task",
     
     "description": """
-    Tangent Systems LLC
-    ===================
-    Tangent systems is a service based company that does not manage any inventory. Tangent systems will use Sales Orders to create Tasks inside of Projects. 
-    They will manage the delivery of those services through the projects app. Tangent Systems has a large number of billable services (tasks) that they manage. 
-    In previous ERP softwares Tangent Systems has had problems in loosing track of which services have been billed for. They do not bill based on the time it takes,
-    rather on deliverables, which makes automation more difficult.
-
-    Tangent Systems would like for draft invoices to be created based on the completion of tasks. This would help make sure they are not missing out on any billable services.
+Tangent Systems would like for draft invoices to be created based on the completion of tasks. This would help make sure they are not missing out on any billable services.
     """,
     
     "version": "1.0.0",
@@ -23,7 +16,6 @@
     "depends": ["base"],
     
     "data": [
-
     ],
     
     
